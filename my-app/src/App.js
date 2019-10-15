fda0da882e83bc2d35c607e1584ed99dff939587
Archish Arun <archisharun99@gmail.com>
2019-10-14 23:08:30 -0700

    update app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/nav.jsx'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div id="colorlib-page">
+        <div id="container-wrap">
+         	<Nav></Nav>
+				<div id="colorlib-main">
+          	</div>
+      	</div>
+      </div>
+    );
+  }
 }
 
 export default App;
